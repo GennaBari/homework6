3b57a5db88415d365e01edec3e231659a316cf75
GennaBari <barinovaevgenia84@gmail.com>
2024-07-17 13:16:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+my_dict = {'Genny': 1984,'Vlad': 2010, 'Nika': 2015, 'Alex': 1984}
+print(my_dict)
+my_dict['Genny'] = 1984
+print(my_dict['Genny'])
+print (my_dict.get('Irakliy'))
+my_dict.update({'Sasha': 1936,
+                'Ivan': 1956})
+b = my_dict.pop ('Sasha')
+print(b)
+my_dict = {'Genny': 1984,'Vlad': 2010, 'Nika': 2015, 'Alex': 1984,'Ivan': 1956}
+print(my_dict)
+
+my_set = {8, 5, 2, 2, 5, 8, 'Mum', (3, 6, 9)}
+print(my_set)
+my_set.add(7)
+my_set.add(6)
+my_set.remove (8)
+print(my_set)
+
+
+
